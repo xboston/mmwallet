5d024354b0f2d2c2f9347bb5136d58cef60e978b
boston <bost56@gmail.com>
2023-02-08 00:46:33 +0800

    fix: deepsource#JS-0116@2

--- src/routes/api/app/+server.ts
+++ src/routes/api/app/+server.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
-export const GET: RequestHandler = async (event) => {
+export const GET: RequestHandler = (event) => {
     return json({
         latests: {
             ios: event.locals.latests.ios,
--- src/routes/api/latest/+server.ts
+++ src/routes/api/latest/+server.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
-export const GET: RequestHandler = async (event) => {
+export const GET: RequestHandler = (event) => {
     return json({
         latests: event.locals.latests,
         updatedOn: event.locals.updatedAt,
